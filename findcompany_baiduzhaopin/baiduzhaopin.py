132fdefcab5ee26b83c7fb2e4d5105ea08036618
azuse <misakaxindex@gmail.com>
2019-01-28 16:50:01 +0800

    update

--- findcompany_baiduzhaopin/baiduzhaopin.py
+++ findcompany_baiduzhaopin/baiduzhaopin.py
@@ -9,29 +9,19 @@ import pymysql
 import time
 import random
 import string
+from bs4 import BeautifulSoup
+import re
+import sys
 
-db_username = "root"
-db_password = "misakaxindex"
-db_dbname = "findcompany"
+time_sleep = 1
 
-time_sleep = 0.5
-
-## 失败处理 超过20次休眠90秒
+# 失败处理 超过20次休眠90秒
 fail_count = 0
-fail_count_limit = 20
-fail_sleep = 90
+fail_count_limit = 10
+fail_sleep = 900
 
 # 百度招聘mysql插入函数
 
-def fail():
-    global fail_count
-    global fail_count_limit
-    global fail_sleep
-    fail_count = fail_count + 1
-    if fail_count >= fail_count_limit:
-        print("fail too much, going to sleep a while")
-        time.sleep(fail_sleep)
-        fail_count = 0
 
 class mysql_baiduzhaopin:
 
@@ -95,15 +85,64 @@ class mysql_baiduzhaopin:
         except:
             return 0
 
+    # 将查到的company官网链接插入数据库
+    def insertCompany(self, company, cid, url):
+        sql = """INSERT INTO company (cid, company, url) VALUES ('{0}', '{1}'  ,'{2}');""" .format(
+            cid, company, url)
+        self.cursor.execute(
+            "SELECT COUNT(cid) FROM company WHERE cid LIKE '{0}';".format(cid))
+        if self.cursor._rows[0] != (0,):
+            return 0
 
-def getToken():
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+            return 1
+        except:
+            return 0
+
+
+db_username = "root"
+db_password = "misakaxindex"
+db_dbname = "findcompany"
+db = mysql_baiduzhaopin(db_username, db_password, db_dbname)
+
+
+def fail():
+    global fail_count
+    global fail_count_limit
+    global fail_sleep
+    fail_count = fail_count + 1
+    if fail_count >= fail_count_limit:
+        print("fail too much, going to sleep a while")
+        time.sleep(fail_sleep)
+        fail_count = 0
+
+
+def getToken(query, city):
+    global time_sleep
+
     print("获取token")
-    url = "https://zhaopin.baidu.com/quanzhi"
+    url = "http://zhaopin.baidu.com/quanzhi?query=" + \
+        parse.quote(query)+"&city="+parse.quote(city)
+    headers = {"Accept": "*/*",
+               "Accept-Encoding": "gzip, deflate, br",
+                   "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+                   "Connection": "keep-alive",
+                   "Cookie": "BAIDUID=D218FD911DA27FA9E5C8A9B6B3F62C92:FG=1; BIDUPSID=D218FD911DA27FA9E5C8A9B6B3F62C92; PSTM=1548381180; delPer=0; PSINO=5; H_PS_PSSID=26525_1443_21105_28328_22158",
+                   "DNT": "1",
+                   "Host": "zhaopin.baidu.com",
+                   "Referer": "http://zhaopin.baidu.com/quanzhi?query="+parse.quote(query)+"&city="+parse.quote(city),
+                   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36}"
+               }
+
     proxies = {"http": "http://child-prc.intel.com:913",
                "https": "http://child-prc.intel.com:913"}
     while 1:
         try:
-            r = requests.get(url, proxies=proxies)
+            r = requests.get(url, headers=headers, proxies=proxies)
+            time.sleep(time_sleep)
+
             break
         except:
             print("获取token时网络错误,重试")
@@ -117,30 +156,37 @@ def getToken():
     # reverse token
     token = token[::-1]
     token = parse.quote(token)
-    url = "https://zhaopin.baidu.com/api/news?type=5&token="+token
+    url = "http://zhaopin.baidu.com/api/news?type=5&token="+token
     try:
         r = requests.get(url, proxies=proxies)
+        time.sleep(time_sleep)
+
     except:
         pass
     print("new token:"+token)
     return token
 
+# =========================
+# | 百度招聘api查询函数      |
+# | query   查询关键词      |
+# | num     查询多少个职位   |
+# =========================
 
-# 百度招聘api查询函数
-# query 查询关键词
-# num 查询多少个职位
+
 def baiduzhaopin(query, city, num):
     # rn意义不明 也许是每次返回的结果数？
     rn = 10
     # pn返回第pn个开始的结果 pn/10相当于 页
-    pn = 0
-    token = getToken()
+    pn = 10
+    token = getToken(query, city)
     while pn < num:
 
+        # #####IMPORTANT#####
         # token 必须要有 不让强制搜索"招聘",但是token的值无所谓
         # api后台有校验,校验过程不清楚,校验失败query会强制变成"招聘"
+        # 即使token设置正确，抓取数据的频率过高也会被ban，ban的应该是ip
 
-        url = "https://zhaopin.baidu.com/api/qzasync?query=" + parse.quote(query) + "&city=" + parse.quote(
+        url = "http://zhaopin.baidu.com/api/qzasync?query=" + parse.quote(query) + "&city=" + parse.quote(
             city) + "&is_adq=1&pcmod=1&token=" + token + "&pn=" + str(pn) + "&rn=" + str(rn) + "&sort_type=1&sort_key=5"
 
         headers = {"Accept": "*/*",
@@ -150,7 +196,7 @@ def baiduzhaopin(query, city, num):
                    "Cookie": "BAIDUID=D218FD911DA27FA9E5C8A9B6B3F62C92:FG=1; BIDUPSID=D218FD911DA27FA9E5C8A9B6B3F62C92; PSTM=1548381180; delPer=0; PSINO=5; H_PS_PSSID=26525_1443_21105_28328_22158",
                    "DNT": "1",
                    "Host": "zhaopin.baidu.com",
-                   "Referer": "https://zhaopin.baidu.com/quanzhi?query=%E8%AE%A1%E7%AE%97%E6%9C%BA&city=%E4%B8%8A%E6%B5%B7",
+                   "Referer": "https://zhaopin.baidu.com/quanzhi?query="+parse.quote(query)+"&city="+parse.quote(city),
                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36}"
                    }
 
@@ -169,10 +215,7 @@ def baiduzhaopin(query, city, num):
 
         rdata = json.loads(r.text)
 
-        global db_usernamedb_username
-        global db_passworddb_username
-        global db_dbname
-        db = mysql_baiduzhaopin(db_username, db_password, db_dbname)
+        global db
         if rdata['data']['errno'] == -1:
             print("数据获取失败,可能是已经取得了所有结果")
             pn = pn + 10
@@ -182,7 +225,7 @@ def baiduzhaopin(query, city, num):
         if hilight != query:
             print("token失效,或抓取受到限制")
             fail()
-            token = getToken()
+            token = getToken(query, city)
             continue
 
         # 新插入的条目计数
@@ -197,41 +240,110 @@ def baiduzhaopin(query, city, num):
     return "查询结束"
 
 
-ret = baiduzhaopin("计算机视觉", "深圳", 1000)
-print(ret)
-ret = baiduzhaopin("计算机","深圳", 1000)
-print(ret)
-ret = baiduzhaopin("物联网","深圳", 1000)
-print(ret)
-ret = baiduzhaopin("智能","深圳", 1000)
-print(ret)
-ret = baiduzhaopin("无人","深圳", 1000)
-print(ret)
-ret = baiduzhaopin("高科技","深圳", 1000)
-print(ret)
+# ==================================
+# |使用百度百聘的company id寻找公司信息 |
+# ==================================
+def baiducompany(cid, company):
+    url = "http://zhaopin.baidu.com/company?query=" + \
+        cid+"&page=registration&city=%E4%B8%8A%E6%B5%B7"
 
-ret = baiduzhaopin("计算机视觉", "北京", 1000)
-print(ret)
-ret = baiduzhaopin("计算机","北京", 1000)
-print(ret)
-ret = baiduzhaopin("物联网","北京", 1000)
-print(ret)
-ret = baiduzhaopin("智能","北京", 1000)
-print(ret)
-ret = baiduzhaopin("无人","北京", 1000)
-print(ret)
-ret = baiduzhaopin("高科技","北京", 1000)
-print(ret)
+    headers = {"Accept": "*/*",
+               "Accept-Encoding": "gzip, deflate, br",
+                   "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+                   "Connection": "keep-alive",
+                   "Cookie": "BAIDUID=D218FD911DA27FA9E5C8A9B6B3F62C92:FG=1; BIDUPSID=D218FD911DA27FA9E5C8A9B6B3F62C92; PSTM=1548381180; delPer=0; PSINO=5; H_PS_PSSID=26525_1443_21105_28328_22158",
+                   "DNT": "1",
+                   "Host": "zhaopin.baidu.com",
+                   "Referer": "https://zhaopin.baidu.com/quanzhi?query=%E8%AE%A1%E7%AE%97%E6%9C%BA&city=%E4%B8%8A%E6%B5%B7",
+                   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36}"
+               }
 
-ret = baiduzhaopin("计算机视觉", "上海", 1000)
-print(ret)
-ret = baiduzhaopin("计算机","上海", 1000)
-print(ret)
-ret = baiduzhaopin("物联网","上海", 1000)
-print(ret)
-ret = baiduzhaopin("智能","上海", 1000)
-print(ret)
-ret = baiduzhaopin("无人","上海", 1000)
-print(ret)
-ret = baiduzhaopin("高科技","上海", 1000)
-print(ret)
+    proxies = {"http": "http://child-prc.intel.com:913",
+               "https": "http://child-prc.intel.com:913"}
+
+    try:
+        r = requests.get(url, proxies=proxies)
+
+        global time_sleep
+        time.sleep(time_sleep)
+    except:
+        print("request timeout")
+
+    # r.encoding = 'utf8'
+    soup = BeautifulSoup(r.text, features="html.parser")
+
+    ret = soup.select('div:contains("企业官网").line-equal-item')
+    if len(ret) == 0:
+        print(company + " not found")
+        return -1
+    companyUrl = ret[0].text.split("企业官网")[1]
+
+    ret = soup.select('title')
+    companyName = ret[0].text.split("-企业名片-")[0]
+
+    global db
+    db.insertCompany(companyName, cid, companyUrl)
+
+    print(companyName + companyUrl + " inserted!")
+
+# 自动查找百度招聘数据库里的公司官网
+
+
+def findCompanyUrl():
+    sql = "SELECT `companyId`, `company` FROM `baiduzhaopin` WHERE `companyId` NOT IN (SELECT `cid` FROM `company`) GROUP BY `companyId`,`company` ORDER BY `company` DESC"
+    global db
+    db.cursor.execute(sql)
+    db.db.commit()
+    results = db.cursor.fetchall()
+    for row in results:
+        baiducompany(row[0], row[1])
+
+
+
+if __name__ == "__main__":
+    if(sys.argv[1] == "zhaopin"):
+        ret = baiduzhaopin("计算机视觉", "深圳", 1000)
+        print(ret)
+        ret = baiduzhaopin("计算机", "深圳", 1000)
+        print(ret)
+        ret = baiduzhaopin("物联网", "深圳", 1000)
+        print(ret)
+        ret = baiduzhaopin("智能", "深圳", 1000)
+        print(ret)
+        ret = baiduzhaopin("无人", "深圳", 1000)
+        print(ret)
+        ret = baiduzhaopin("高科技", "深圳", 1000)
+        print(ret)
+
+        ret = baiduzhaopin("计算机视觉", "北京", 1000)
+        print(ret)
+        ret = baiduzhaopin("计算机", "北京", 1000)
+        print(ret)
+        ret = baiduzhaopin("物联网", "北京", 1000)
+        print(ret)
+        ret = baiduzhaopin("智能", "北京", 1000)
+        print(ret)
+        ret = baiduzhaopin("无人", "北京", 1000)
+        print(ret)
+        ret = baiduzhaopin("高科技", "北京", 1000)
+        print(ret)
+
+        ret = baiduzhaopin("计算机视觉", "上海", 1000)
+        print(ret)
+        ret = baiduzhaopin("计算机", "上海", 1000)
+        print(ret)
+        ret = baiduzhaopin("物联网", "上海", 1000)
+        print(ret)
+        ret = baiduzhaopin("智能", "上海", 1000)
+        print(ret)
+        ret = baiduzhaopin("无人", "上海", 1000)
+        print(ret)
+        ret = baiduzhaopin("高科技", "上海", 1000)
+        print(ret)
+    elif(sys.argv[1] == "company"):
+       while 1:
+            findCompanyUrl()
+        
+    else:
+        print("python3 baiduzhaopin.py company -> get company homesite url")
+        print("python3 baiduzhaopin.py zhaopin -> get zhaopin information from baidu zhaopin")
