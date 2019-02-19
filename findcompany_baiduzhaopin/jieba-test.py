427ac2b94256e5017a1750b6721419d073a175af
azuse <misakaxindex@gmail.com>
2019-02-19 16:39:28 +0800

    debug

--- findcompany_baiduzhaopin/jieba-test.py
+++ findcompany_baiduzhaopin/jieba-test.py
@@ -97,73 +97,76 @@ r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 nlp = BosonNLP('1aooLCxg.33225.eMAXko7EljEV')
 
 for item in data:
-    id = item[0]
-    company = item[1]
-    url = item[2]
-    if(url == "None"):
-        continue
-    
-    if(url.find("http://") == -1 and url.find("https://") == -1):
-        url = "http://" + url
-
-    if(re.match(regex, url) is None):
-        continue 
-
-    print("getting url " + url)
-
-    #### using selenium ####
-
-    # driver.get(url)
-    # soup = BeautifulSoup(driver.page_source, features="html.parser")
-    # text = soup.get_text()
-    # btn = driver.find_element_by_css_selector("a")
-    
-    # print("page get daze: "+ text)
-    # jieba_tf_idf(text)
-
-    #### using request ####
     try:
-        r = requests.get(url, timeout = 5)
-        r.encoding = "utf-8"
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
+        id = item[0]
+        company = item[1]
+        url = item[2]
+        if(url == "None"):
+            continue
+        
+        if(url.find("http://") == -1 and url.find("https://") == -1):
+            url = "http://" + url
 
-        continue
-    soup = BeautifulSoup(r.text,  features="html.parser")
-    tagArray = ["p", "h1", "h2", "h3", "h4",  "h5", "h6", "label", "a", "span"]
-    text = ""
-    for i in range(len(tagArray)):
-        res = soup.find_all(tagArray[i])
-        for j in range(len(res)):
-            text += "," +  res[j].get_text()
-    
-    a_all = soup.find_all("a")
-    for k in range(len(a_all)):
-        href = a_all[k].attrs.get('href', "")
-        if(href.find("http://") == -1 and href.find("https://") == -1):
-            href = url + href
-
-
-        if(re.match(regex, href) is None):
+        if(re.match(regex, url) is None):
             continue 
 
-        print("getting href " + href)
+        print("getting url " + url)
 
+        #### using selenium ####
+
+        # driver.get(url)
+        # soup = BeautifulSoup(driver.page_source, features="html.parser")
+        # text = soup.get_text()
+        # btn = driver.find_element_by_css_selector("a")
+        
+        # print("page get daze: "+ text)
+        # jieba_tf_idf(text)
+
+        #### using request ####
         try:
-            r_sec = requests.get(href, timeout = 5)
-            r_sec.encoding = "utf-8"
+            r = requests.get(url, timeout = 5)
+            r.encoding = "utf-8"
         except:
             print("Unexpected error:", sys.exc_info()[0])
+
             continue
-        soup_sec = BeautifulSoup(r_sec.text, features="html.parser")
+        soup = BeautifulSoup(r.text,  features="html.parser")
+        tagArray = ["p", "h1", "h2", "h3", "h4",  "h5", "h6", "label", "a", "span"]
+        text = ""
         for i in range(len(tagArray)):
             res = soup.find_all(tagArray[i])
             for j in range(len(res)):
-                text += "," + res[j].get_text()
+                text += "," +  res[j].get_text()
+        
+        a_all = soup.find_all("a")
+        for k in range(len(a_all)):
+            href = a_all[k].attrs.get('href', "")
+            if(href.find("http://") == -1 and href.find("https://") == -1):
+                href = url + href
 
-  
 
-    print("page get daze: "+ text)
-    if text != "" and r.status_code == 200:
-        jieba_tf_idf(text, company)
+            if(re.match(regex, href) is None):
+                continue 
+
+            print("getting href " + href)
+
+            try:
+                r_sec = requests.get(href, timeout = 5)
+                r_sec.encoding = "utf-8"
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                continue
+            soup_sec = BeautifulSoup(r_sec.text, features="html.parser")
+            for i in range(len(tagArray)):
+                res = soup.find_all(tagArray[i])
+                for j in range(len(res)):
+                    text += "," + res[j].get_text()
+
+    
+
+        print("page get daze: "+ text)
+        if text != "" and r.status_code == 200:
+            jieba_tf_idf(text, company)
+    except: 
+        continue
         
